008b1f014ecd336670460511ee3c920b0941a7cf
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-11-07 11:04:52 +0600

    ok

--- Acadamy/Data Types Variables.java
+++ Acadamy/Data Types Variables.java
@@ -1,30 +1,13 @@
 package Acadamy;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 class DataTypesVariables {
     public static void main(String[] args) {
-        // System.out.println("Hello World");
-        // int a = 25;
-        // int b = 10;
-        // System.out.println(a + b);
-        // b = 20;
-        // System.out.println(b);
-        // int mul = a * b;
-        // System.out.println(mul); //25 *20 = 500;
-        // System.out.println((a * b) / (a - b));
-
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Your 1st Number: ");
-        int num = sc.nextInt();
-        
-        System.out.print("Enter Your 2nd Number: ");
-        int num1 = sc.nextInt();
-
-        int sum = num + num1;
-
-        System.out.print("Total Sum is :" + sum);
+        String name = "Arrad";
+        String depertmentName = "CSE";
+        String myId = "CSE07908432";
+        System.out.println("My Name is " + name + "My Depertment Name is  " + depertmentName + "My Id is" + myId);
 
 
 
